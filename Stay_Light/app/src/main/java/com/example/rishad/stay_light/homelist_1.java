052dc65db4a50b618c0bb1f1417aacb6370c8d14
Shahriar <shahriarnayeem58@gmail.com>
2018-09-16 21:53:27 +0600

    Dropdown added

--- Stay_Light/app/src/main/java/com/example/rishad/stay_light/homelist_1.java
+++ Stay_Light/app/src/main/java/com/example/rishad/stay_light/homelist_1.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class homelist_1 extends AppCompatActivity {
+public class homelist_1 extends AppCompatActivity  {
 
     private Button nextButton;
+    Spinner dropdown;
+    private String[] houseType = new String[]{"Entire Place", "Private Room", "Shared Room"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,13 @@ public class homelist_1 extends AppCompatActivity {
         setContentView(R.layout.activity_homelist_1);
 
         nextButton = findViewById(R.id.next);
+        dropdown = findViewById(R.id.typeofhouse);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, houseType);
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(typesOfhouse);
+        dropdown.setSelection(0);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,4 +37,17 @@ public class homelist_1 extends AppCompatActivity {
             }
         });
     }
+
+    AdapterView.OnItemSelectedListener typesOfhouse = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long l) {
+            String item = parent.getItemAtPosition(pos).toString();
+            Toast.makeText(homelist_1.this, item, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
 }
