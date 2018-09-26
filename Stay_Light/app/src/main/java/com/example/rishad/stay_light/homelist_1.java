7ae1b317853d53bf1bbf2f28a7a1b1f5a24f6195
Shahriar <shahriarnayeem58@gmail.com>
2018-09-26 22:29:55 +0600

    Commit

--- Stay_Light/app/src/main/java/com/example/rishad/stay_light/homelist_1.java
+++ Stay_Light/app/src/main/java/com/example/rishad/stay_light/homelist_1.java
@@ -8,21 +8,31 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class homelist_1 extends AppCompatActivity  {
 
+    private EditText noOfGuest,houseLocation;
     private Button nextButton;
     Spinner dropdown;
-    private String[] houseType = new String[]{"","Entire Place", "Private Room", "Shared Room"};
+    private String[] houseType = new String[]{"","Apartment", "Duplex", "Bread And Breakfast"};
+    public String houseTypeItem;
+
+    {
+        houseTypeItem = new String();
+    }
+    public String guestNumber,location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homelist_1);
 
+        houseLocation = findViewById(R.id.location);
+        noOfGuest = findViewById(R.id.noOfguest);
         nextButton = findViewById(R.id.next);
         dropdown = findViewById(R.id.typeofhouse);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, houseType);
@@ -31,7 +41,8 @@ public class homelist_1 extends AppCompatActivity  {
         dropdown.setSelection(0);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-
+        guestNumber = noOfGuest.getText().toString();
+        location = houseLocation.getText().toString();
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,8 +55,8 @@ public class homelist_1 extends AppCompatActivity  {
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int pos, long l) {
             ((TextView) parent.getChildAt(0)).setTextColor(Color.BLUE);
-            String item = parent.getItemAtPosition(pos).toString();
-            Toast.makeText(homelist_1.this, item, Toast.LENGTH_SHORT).show();
+            houseTypeItem = parent.getItemAtPosition(pos).toString();
+            Toast.makeText(homelist_1.this, houseTypeItem, Toast.LENGTH_SHORT).show();
         }
 
         @Override
--- Stay_Light/app/src/main/java/com/example/rishad/stay_light/homelist_2.java
+++ Stay_Light/app/src/main/java/com/example/rishad/stay_light/homelist_2.java
@@ -1,20 +1,34 @@
 package com.example.rishad.stay_light;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class homelist_2 extends AppCompatActivity {
 
     private Button button;
-
+    Spinner dropdownAccomodation;
+    private String[] accoTypeList = new String[]{"","Entire Place", "Private Room", "Shared Room"};
+    public String accoType = new String();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homelist_2);
+
+        dropdownAccomodation = findViewById(R.id.typeofproperty);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, accoTypeList);
+        dropdownAccomodation.setAdapter(adapter);
+        dropdownAccomodation.setOnItemSelectedListener(AcooType);
+        dropdownAccomodation.setSelection(0);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         button = findViewById(R.id.next);
 
@@ -25,4 +39,18 @@ public class homelist_2 extends AppCompatActivity {
             }
         });
     }
+
+    AdapterView.OnItemSelectedListener AcooType = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long l) {
+            ((TextView) parent.getChildAt(0)).setTextColor(Color.BLUE);
+            accoType = parent.getItemAtPosition(pos).toString();
+            Toast.makeText(homelist_2.this, accoType, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
 }
